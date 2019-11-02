20eea28af04ee97caab933dec3984fbf715fcd53
Ajit Krishna <ajit.namakkal@bc.group>
2019-11-02 21:35:00 +0800

    Updates

---
+++ precache-manifest.9764d197194658bb7f87559f102a523e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbfe4207d14470782d2deb823a4ea10d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "995a7fde9e8663d09947",
+    "url": "/static/css/main.2ffe6f70.chunk.css"
+  },
+  {
+    "revision": "65c9c3acbdd359bb2585",
+    "url": "/static/js/2.a6a48418.chunk.js"
+  },
+  {
+    "revision": "995a7fde9e8663d09947",
+    "url": "/static/js/main.baf443c4.chunk.js"
+  },
+  {
+    "revision": "f351487323c49610c172",
+    "url": "/static/js/runtime-main.1e28957e.js"
+  },
+  {
+    "revision": "c7c317d8f0f74a36ae3bdf1388829998",
+    "url": "/static/media/XtractIcon.c7c317d8.png"
+  },
+  {
+    "revision": "a742235e282a03464bb81b6a46ae74e4",
+    "url": "/static/media/fillForm.a742235e.svg"
+  },
+  {
+    "revision": "106864a8ab7ca5d40e338e54d36b0721",
+    "url": "/static/media/getAccount.106864a8.svg"
+  },
+  {
+    "revision": "1b510958fca4b00e2e529b3c092e4cb9",
+    "url": "/static/media/uploadDoc.1b510958.svg"
+  }
+]);
